4d6131021ff1d1129c678d44d133c4b282d63808
Misha <lumenhorizon@icloud.com>
2023-08-05 11:07:51 +0700

    Create .cpp for Battlefield

---
+++ src/battleships/sources/Battlefield.cpp
@@ -0,0 +1,25 @@
+#include <Battlefield.hpp>
+
+namespace bs
+{
+    Battlefield::Battlefield()
+    {
+    }
+
+    bool Battlefield::tryPlaceShip(Ship ship)
+    {
+    }
+
+    TileState **Battlefield::getField()
+    {
+    }
+
+    void Battlefield::shoot()
+    {
+    }
+
+    bool Battlefield::shoot(Vector2i position)
+    {
+    }
+
+} // namespace bs
