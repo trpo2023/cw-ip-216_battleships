1f56e6eee23ed631df6131f990c58b03a29d0628
Misha <lumenhorizon@icloud.com>
2023-08-08 12:24:24 +0700

    Create getRandomVector fun

--- src/battleships/headers/Vector2i.hpp
+++ src/battleships/headers/Vector2i.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdlib.h>
+
 namespace bs
 {
     struct Vector2i
@@ -8,6 +10,7 @@ namespace bs
         int y = 0;
 
         void makeOffset(int value, int squareSize);
+        static Vector2i getRandomVector(int maxX, int maxY);
         Vector2i() {}
         Vector2i(int x, int y) : x(x), y(y) {}
     };
