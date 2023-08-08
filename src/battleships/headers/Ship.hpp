ea641e77e678ca11c8879cbaecfe99262a0eaf31
Misha <lumenhorizon@icloud.com>
2023-08-08 19:43:45 +0700

    Added getPositonsSet methods

--- src/battleships/headers/Ship.hpp
+++ src/battleships/headers/Ship.hpp
@@ -2,6 +2,7 @@
 
 #include <Vector2i.hpp>
 #include <Rectangle2i.hpp>
+#include <set>
 
 namespace bs
 {
@@ -12,8 +13,12 @@ namespace bs
         Vector2i startPosition;
 
         Vector2i getEndPosition();
+
         Rectangle2i getAreaRectangle();
         Rectangle2i getBodyRectangle();
+
+        std::set<Vector2i> getBodyPositionsSet();
+        std::set<Vector2i> getAreaOutlinePositionsSet();
 
         Ship(Vector2i startPosition, int length, bool isHorizontal)
             : startPosition(startPosition),
