dea495d8399cd021cd9e886136c637ec93740d95
Misha <lumenhorizon@icloud.com>
2023-08-08 20:14:29 +0700

    Implement ship destroy logic

--- src/battleships/headers/Battlefield.hpp
+++ src/battleships/headers/Battlefield.hpp
@@ -14,18 +14,21 @@ namespace bs
     private:
         TileState **field;
         std::list<Ship> ships;
+        FieldChanges currentChanges;
 
         bool checkShipArea(Rectangle2i area);
         bool checkShipOutOfBorders(Ship ship);
         bool checkShipPlace(Ship ship);
-
         void addShipToField(Ship ship);
-
         bool tryPlaceShip(Ship ship);
         void placeShip(int size);
 
         bool checkShootPosition(Vector2i position);
         void shoot(Vector2i position);
+        Ship getShipByPosition(Vector2i position);
+        void hitShip(Vector2i position);
+        bool checkShipDestroyed(Ship ship);
+        void destroyShip(Ship ship);
 
     public:
         Event<FieldChanges> onFieldChanged;
