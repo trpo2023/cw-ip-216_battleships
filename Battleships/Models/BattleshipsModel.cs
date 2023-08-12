3ae0f09eaed06a6ff7a01307fd3e2e87db9df6a9
Lumen <123202753+Lumen1024@users.noreply.github.com>
2023-08-12 12:46:50 +0700

    Implement BattleshipsModel

---
+++ Battleships/Models/BattleshipsModel.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+
+using Battleships.Models.Changes;
+using Battleships.Models.Primitive;
+
+namespace Battleships.Models;
+public class BattleshipsModel
+{
+    private Battlefield _playerField;
+    private Battlefield _enemyField;
+
+    private FieldsChanges _currentChanges;
+
+    public delegate void GameOverHandler(bool isPlayerWin);
+    public event GameOverHandler OnGameOver;
+
+    public delegate void FieldsChangedHandler(FieldsChanges fieldsChanges);
+    public event FieldsChangedHandler OnFieldsChanged;
+    public BattleshipsModel()
+    {
+        _playerField = new();
+        _enemyField = new();
+
+        _playerField.OnFieldChanged += (List<TileChanges> changes) =>
+        {
+            _currentChanges.playerChanges = changes;
+        };
+
+        _enemyField.OnFieldChanged += (List<TileChanges> changes) =>
+        {
+            _currentChanges.enemyChanges = changes;
+        };
+    }
+
+    public TileState[,] GetTileField(FieldType fieldType)
+    {
+        return fieldType switch
+        {
+            FieldType.Player => _playerField.GetField(),
+            FieldType.Enemy => _enemyField.GetField(),
+            _ => null,
+        };
+    }
+
+    public bool TryShoot(Vector2i position)
+    {
+        if (_enemyField.TryShoot(position))
+            return false;
+        _playerField.ShootRandom();
+        OnFieldsChanged.Invoke(_currentChanges);
+        return true;
+    }
+
+}
