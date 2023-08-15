4601ac8a323bb80a8f0c62a00c3649d794d8e051
Lumen <123202753+Lumen1024@users.noreply.github.com>
2023-08-15 22:17:26 +0700

    Create GetOutlinePositionsSetTest

--- BattleshipsTest/Models/RectangleTest.cs
+++ BattleshipsTest/Models/RectangleTest.cs
@@ -69,7 +69,30 @@ public class RectangleTest
     [TestMethod]
     public void GetOutlinePositionsSetTest()
     {
+        // #*###
+        // #####
+        // #####
+        // ####*
+        var rect = new Rectangle(new Vector2i(1, 0), new Vector2i(4, 3));
+        var actualSet = rect.GetOutlinePositionsSet();
+        HashSet<Vector2i> outlineset = new() {
+            new Vector2i(1,0),
+            new Vector2i(2,0),
+            new Vector2i(3,0),
+            new Vector2i(4,0),
 
+            new Vector2i(1,1),
+            new Vector2i(1,2),
+            new Vector2i(4,1),
+            new Vector2i(4,2),
+
+            new Vector2i(1,3),
+            new Vector2i(2,3),
+            new Vector2i(3,3),
+            new Vector2i(4,3),
+        };
+
+        Assert.IsTrue(actualSet.SetEquals(outlineset));
     }
 
 }
