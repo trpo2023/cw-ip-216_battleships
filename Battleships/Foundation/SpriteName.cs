9b9b600c7a9bcafc5b736cb768a3ae8d51816012
Lumen <123202753+Lumen1024@users.noreply.github.com>
2023-08-20 21:46:21 +0700

    Create Sprite class

---
+++ Battleships/Foundation/SpriteName.cs
@@ -0,0 +1,11 @@
+namespace Battleships.Foundation;
+
+public enum SpriteName
+{
+    End_background,
+    Game_background,
+    Menu_background,
+    Exit_button,
+    Play_button,
+    Menu_title,
+}
